2d9c62aa396fe5573f09ef009d9dd63c2fad9cd4
DESKTOP-MRQ45D3\User <kawhi060941@gmail.com>
2024-05-14 17:22:21 +0800

    icon package install

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router.js'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas)
+
 
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .mount('#app')
