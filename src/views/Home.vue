a63cbfd6101de331d0298a7e2b371a7d76a66b58
DESKTOP-MRQ45D3\User <kawhi060941@gmail.com>
2024-05-15 16:46:51 +0800

    home參考

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,92 @@
 <template>
 
+    <body>
+        <!-- 不用了 -->
+        <!-- <header>
+            HEADER
+        </header> -->
+        <div class="main">
+            <aside class="left">
+                LEFT
+            </aside>
+            <main>
+                MAIN CONTENT
+            </main>
+            <aside class="right">
+                RIGHT
+            </aside>
+        </div>
+    </body>
+    <footer>
+        https://www.youtube.com/watch?v=oeOQQT7Xn7w
+        footer
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto cumque recusandae, perferendis asperiores
+        esse obcaecati? Nisi a eveniet temporibus. Delectus est provident inventore ex velit. Unde corporis quos iste.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloremque praesentium quis numquam nisi
+        enim expedita explicabo error voluptatum, vel molestiae amet accusantium voluptate accusamus libero hic veniam
+        deleniti. Voluptates!
+        footer
+        footer
+    </footer>
 </template>
-
-<script setup lang='ts'>
-
+<script setup>
 </script>
+<style scoped>
+* {
+    margin: 1px;
+    box-sizing: border-box;
+}
 
-<style></style>
+body {
+    height: 95vh;
+    background: #34e7e4;
+    font-family: sans-serif;
+    text-align: center;
+    color: black;
+    font-size: 16px;
+    display: flex;
+    flex-direction: column;
+}
+
+header {
+    background: yellow;
+    padding: 2em 0 2em 0;
+}
+
+.left {
+    background: gainsboro;
+    padding: 3em 0 3em 0;
+    flex: 2 2 100px
+}
+
+main {
+    background: white;
+    padding: 3em 0 3em 0;
+    flex: 8 8 150px
+}
+
+.main {
+    display: flex;
+    flex: 1
+}
+
+.right {
+    background: gainsboro;
+    padding: 3em 0 3em 0;
+    flex: 2 2 100px
+}
+
+footer {
+    background: burlywood;
+}
+
+@media all and (max-width: 550px) {
+    .main {
+        flex-direction: column;
+    }
+
+    main {
+        padding: 5em 0 5em 0;
+    }
+}
+</style>
