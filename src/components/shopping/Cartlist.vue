7cf81f6f98f606e2f4d45b45e761680b08548ac5
DESKTOP-MRQ45D3\User <kawhi060941@gmail.com>
2024-05-15 13:35:12 +0800

    修改可購買可不購買判斷

--- src/components/shopping/Cartlist.vue
+++ src/components/shopping/Cartlist.vue
@@ -15,19 +15,26 @@
                 style="cursor: pointer; color: red;" />
         </td>
     </tr>
-    <td v-if="stock < quantity && checked === true" style="color: red;">{{ abc() }}</td>
+    <td v-if="stock < quantity && checked === true">{{ abc() }}</td>
+    <td v-if="stock >= quantity && checked === true">{{ def() }}</td>
 </template>
 <script setup>
 const props = defineProps(["name", "quantity", "productprice", "options", "productid", "memberid", "checked", "stock"]);
-const emits = defineEmits(["cliick", "cliick2", "move", "method-result"]);
+const emits = defineEmits(["cliick", "cliick2", "move", "method-result", "method-resultt"]);
 function cliick3(productid) {
     emits("move", productid)
 }
 //這個方法成立是不能按
 function abc() {
     console.log("abc 方法被執行了");
-    const result = "disabled";
+    const result = true;
     emits("method-result", result); // 發射自定義事件，將方法返回的值傳遞給父元件
+}
+//這個方法成立才能下單
+function def() {
+    console.log("def 方法被執行了");
+    const result = false;
+    emits("method-resultt", result); // 發射自定義事件，將方法返回的值傳遞給父元件
 }
 </script>
 <style></style>
